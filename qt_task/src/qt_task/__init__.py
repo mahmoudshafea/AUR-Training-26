297547988a208293e342592d948df0536b2660a6
mahmoudshafea <es.Mahmoud.shafea2023@alexu.edu.eg>
2026-09-21 09:48:45 +0300

    Qt task done

--- qt_task/src/qt_task/__init__.py
+++ qt_task/src/qt_task/__init__.py
@@ -1,2 +1,7 @@
+from PySide6.QtWidgets import QApplication
+from qt_task.window import Window
 def main() -> None:
-    print("Hello from qt-task!")
+    app=QApplication()
+    window=Window()
+    window.show()
+    app.exec()
--- qt_task/src/qt_task/buttons.py
+++ qt_task/src/qt_task/buttons.py
@@ -5,10 +5,43 @@ class Buttons(QWidget):
     start=Signal()
     pause=Signal()
     reset=Signal()
-    def __init__(self,parent:QWidget|None=None):
+    def __init__(self):
+        super().__init__()
         self._start_button=QPushButton("START")
         self._reset_button=QPushButton("RESET")
-        self._start_button.clicked.connect()
-        self._reset_button.clicked.connect()
+        self.__timer_paused=True
 
+        self._layout=QHBoxLayout(self)
+        self._layout.addWidget(self._start_button)
+        self._layout.addWidget(self._reset_button)
+
+        self._start_button.clicked.connect(self._b1_clicked)
+        self._reset_button.clicked.connect(self._b2_clicked)
+
+        
+
+    @property
+    def timer_paused(self)->bool:
+        return self.__timer_paused
+
+    @timer_paused.setter
+    def timer_paused(self,state:bool)->None:
+        self.__timer_paused = state
+        if self.__timer_paused:
+            self._start_button.setText("START")
+        else:
+            self._start_button.setText("PAUSE")
+
+    def _b1_clicked(self)->None:
+        if self.timer_paused:
+            self.timer_paused=False
+            self.start.emit()
+        else:
+            self.timer_paused=True
+            self.pause.emit()
+
+        
+    def _b2_clicked(self)->None:
+        self.reset.emit()
+        
 
